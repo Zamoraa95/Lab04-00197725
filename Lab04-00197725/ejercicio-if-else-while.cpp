ef27d9f8c3d5ee5a607a9c9022c76ee94edc18b1
Saul <arnoldo.zcea@gmail.com>
2025-10-03 23:29:13 -0600

    ejercicio else if terminado

--- Lab04-00197725/ejercicio-if-else-while.cpp
+++ Lab04-00197725/ejercicio-if-else-while.cpp
@@ -1,28 +1,62 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    string usuario, contra, manolo, wasd;
-    int x, y, resultados, opcion;
+string usuario, clave;
+string usuario_valido="Manolo";
+string clave_valida="wasd";
+float x, y, resultado;
+int opcion;
+int intentos = 3;
 
-    do{
-      cout << "ingrese su usuario:\n" <<endl;  
-      cin >> usuario;
-      cout << "ingrese su clave:" <<endl;
-      cin >> contra;
-    } while(usuario != manolo);(contra != wasd);
-    
+while(intentos > 0){
+  cout << "Ingrese su usuario: "<<endl;
+  cin >> usuario;
+  cout << "Ingrese su clave de acceso: "<<endl;
+  cin >> clave;
 
-    cout << "Sus credenciales son correctas" <<endl;
-    cout << "Elija que operacion desea realizar\n";
-    cout << "1.Sumar\n";
-    cout << "2.Restar\n";
-    cout << "3.Multiplicar\n";
-    cout << "4.Dividir\n";
-    cout << "5.Salir\n";    
-    cout << "Digite su opcion: ";
-    cin >> opcion;
+  if(usuario == usuario_valido && clave == clave_valida){
+    cout << "---Credenciales correctas---" <<endl;
+
+        cout << "1. Sumar\n";
+        cout << "2. Restar\n";
+        cout << "3. Multiplicar\n";
+        cout << "4. Dividir\n";
+        cout << "5. Salir\n";
+        cout << "Seleccione una que operacion desea: ";
+        cin >> opcion;
+        if (opcion >= 1 && opcion <= 4) {
+            cout << "Ingrese el primer numero: ";
+            cin >> x;
+            cout << "Ingrese el segundo numero: ";
+            cin >> y;
+        }if(opcion == 1){
+          resultado = x + y;
+          cout << "El resultado de la operacion es: " << resultado <<endl;
+          break;
+        }else if(opcion == 2){
+          resultado = x - y;
+          cout << "El resultado de la operacion es: " << resultado <<endl;
+          break;
+        }if(opcion == 3){
+          resultado = x * y;
+          cout << "El resultado de la operacion es: " << resultado <<endl;
+          break;
+        }else if(opcion == 4){
+          resultado = x / y;
+          cout << "El resultado de la operacion es: " << resultado <<endl;
+          break;
+        }if(opcion == 5){
+          cout << "Cerrando calculadora"<<endl;
+          break;
+        }
+  } else{
+    cout << "Las credenciales no son correctas" <<endl;
+    intentos--;
+  }
+}
 
 
-    return 0;
+return 0;
 }
